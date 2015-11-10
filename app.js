4e002ba11a5d40e338ed5d75709aedb0869b8c06
Michael Thessel <michael.thessel@gmx.de>
2015-11-10 09:49:44 -1000

    Removed obsolete modules

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 var queue = require('./models/queue.js');
 
 var routes = require('./routes/api');
