1c68093223b6517819af01898f45dc94c78b0caf
Michael Thessel <michael.thessel@gmx.de>
2015-11-10 23:01:54 -1000

    Fixed formatting

--- models/jobs/weather.js
+++ models/jobs/weather.js
@@ -26,29 +26,32 @@ Weather.prototype.getData = function (callback) {
     }
 
     var _this = this;
+    this.hasData = false;
     request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var response = JSON.parse(body),
-            currently = response.currently,
-            minutely = response.minutely,
-            precip, temp;
+        if (!error && response.statusCode == 200) {
+            var response = JSON.parse(body),
+                currently = response.currently,
+                minutely = response.minutely,
+                precip, temp;
 
-        // Get the temperature in celcius
-        temp = Math.round((currently.temperature - 32) * 5 / 9);
+            // Get the temperature in celcius
+            temp = Math.round((currently.temperature - 32) * 5 / 9);
 
-        // Get the chance of precipitation in 15 minutes
-        precip = minutely.data[14].precipProbability * 100;
+            // Get the chance of precipitation in 15 minutes
+            precip = minutely.data[14].precipProbability * 100;
 
-        _this.data = [
-            'Temp: ' + temp + 'C',
-            'Precip: ' + precip + '%',
-        ];
+            _this.data = [
+                'Temp: ' + temp + 'C',
+                'Precip: ' + precip + '%',
+            ];
 
-        // Fetch every 2 minutes, forecast.io has a daily limit of 1000 requests
-        _this.cacheExpiry = Date.now + 60 * 2 * 1000;
+            _this.hasData = true;
 
-        callback(_this);
-      }
+            // Fetch every 2 minutes, forecast.io has a daily limit of 1000 requests
+            _this.cacheExpiry = Date.now + 60 * 2 * 1000;
+
+            callback(_this);
+        }
     });
 }
 
